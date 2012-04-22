e8464fe02ae867f7121d7e47a75e365f180bcc53
Ibrahim Muhammad <ibrahim.mohammad@gmail.com>
2012-04-22 15:11:48 -0400

    topic changes

--- scroller.js
+++ scroller.js
@@ -1,9 +1,10 @@
-var width = 
+var topic_index = 0;
 
 $(document).ready(function() {
-  var topic = data[topics[1]]
-  console.log(topic)
-  showBadge(topics[1], topic)
+  var topic = data[topics[0]];
+  console.log(topic);
+  showBadge(topics[0], topic);
+  scroll();
 })
 
 function showBadge(name, data) {
@@ -18,12 +19,31 @@ function showBadge(name, data) {
 }
 
 function completeBadge(topic) {
-  $("#topic").html(topic1.name+"<br>"); 
-  $("#topic").append('<img class="badge" src="badges/'+ topic1.badge + '">');
+  $("#topic").html(topic.name+"<br>"); 
+  $("#topic").append('<img class="badge" src="badges/'+ topic.badge_completed + '">');
 }
 
 function scroll() {
-  var scroller = document.getElementById('scroller-container')
+  var scroller = document.getElementById('scroller-container');
   scroller.scrollLeft = scroller.scrollLeft + 10;
-  setTimeout( function () { scroll(); }, 70);
+  if (scroller.scrollLeft > getScrollWidth()) nextTopic();
+  if (scroller.scrollLeft > getScrollWidth() - 500) completeBadge(data[topics[topic_index]])
+  setTimeout( function () { scroll(); }, 30);
+}
+
+function getScrollWidth() {
+  var len = $('.exercise').length;
+  var sum = 0;
+  for(i=0;i<len;i++)
+  {
+    sum = sum + $('.exercise')[i].width + 300;
+  }
+  return sum;
+}
+
+function nextTopic() {
+  topic_index = topic_index + 1;
+  var topic = data[topics[topic_index]];
+  showBadge(topics[topic_index], topic);
+  scroller.scrollLeft = 0;
 }
